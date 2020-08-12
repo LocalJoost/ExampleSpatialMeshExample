9e14cb165267588978e6e5bdaa1930a5f1cb38be
Joost van Schaik <joostvanschaik@outlook.com>
2020-08-12 10:13:14 +0200

    Initial checkin code

---
+++ Assets/App/Scripts/InitialSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialSpeed : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _speed;
+    void Start()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = _speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
